ca27dd68d021df8a5f3d2b3bd458db9dda21dc4d
QuinnJL04 <quinnlouie04@gmail.com>
2024-12-03 14:47:17 -0500

    parsed semesters and courses

--- scheduler/image_parser.py
+++ scheduler/image_parser.py
@@ -1,6 +1,7 @@
 import pytesseract
 from PIL import Image
 from pathlib import Path
+import re
 
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
@@ -10,15 +11,50 @@ IMAGE_DIR = BASE_DIR / "output_images"
 print(IMAGE_DIR)
 
 def get_text():
-    text = ""
+    texts = []
     images = list(IMAGE_DIR.glob("*.png"))
-    print(images)
     for image_path in images:
-        print(image_path)
         with Image.open(image_path) as img:
-            text = pytesseract.image_to_string(img)
-        print(text)
+            extracted_text = pytesseract.image_to_string(img)
+            texts.append(extracted_text)
+            print(extracted_text)
+    return "\n".join(texts)
 
-get_text()
+def get_semesters(text):
+    semester_regex = r"^(Spring|Summer 1|Fall|Winter|Summer 2) \d{4} \(\d+\.\d+ Hours\)"
+    semester_headers = re.finditer(semester_regex, text, re.MULTILINE)
+
+    semester_classes = {}
+    previous_header_end = None
+    current_semester = None
+    print(semester_headers)
+    for header_match in semester_headers:
+        if current_semester:
+            classes_text = text[previous_header_end:header_match.start()].strip() #gets all the text between the last processed semester and the current semester
+            semester_classes[current_semester] = classes_text
+        print(header_match)
+        current_semester = header_match.group(0) #gets the string for the matched semester so "Spring 2025 (17.0 hours)"
+        previous_header_end = header_match.end() #If the match is "Spring 2025 (17.0 Hours)"" and it occurs at positions 0 to 24, then header_match.end() returns 25
+
+    if current_semester:
+        semester_classes[current_semester] = text[previous_header_end:].strip()
+    print(semester_classes)
+    return semester_classes
+
+def filter_relevant_classes(semesters):
+    relevant_classes = {}
+    for semester, courses in semesters.items():
+        class_lines = [
+            re.sub(r"^-", "", course.strip())
+            for course in courses.splitlines()
+        ]
+        relevant_classes[semester] = class_lines
+    print(relevant_classes)
+    return relevant_classes
+
+if __name__ == "__main__":
+    text = get_text()
+    semesters = get_semesters(text=text)
+    filter_relevant_classes(semesters=semesters)
 
 
